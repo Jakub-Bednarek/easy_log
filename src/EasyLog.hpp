960262ac603fd76f17701842e4ac59e8334b5189
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-03-07 00:55:26 +0100

    First commit

---
+++ src/EasyLog.hpp
@@ -0,0 +1,12 @@
+#ifndef
+#define EASY_LOG_HPP
+
+namespace Elog
+{
+    class Logger
+    {
+        
+    }
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "O kurwa\n";
+    return 0;
+}
